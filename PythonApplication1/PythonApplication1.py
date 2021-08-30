719f42e8ccc0ae3cf9c85b20383717a53ddf67b7
n0s1s <artem@vavin.su>
2021-08-30 20:50:53 +0300

    эжэ

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -4,15 +4,20 @@ import json
 import pyaudio
 import pyttsx3
 import difflib
-
+import serial
 if not os.path.exists("model-ru"):
     print ("error model import")
     exit (1) #ПРОВЕРКА ИНИЦИАЛИЗАЦИИ РЕЧЕВОЙ МОДЕЛИ ДЛЯ РАСПОЗНАВАНИЯ
+
+
 
 ###########################                                           ### ИНИЦИАЛИЗАЦИЯ МОДУЛЕЙ И КОМПОНЕНТОВ ###
 p = pyaudio.PyAudio()               # ИНИЦИАЛИЗАЦИЯ РАБОТЫ СО ЗВУКОМ
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000) #НАСТРОИЛИ ПОТОК С МИКРОФОНА
 stream.start_stream()               # НАЧАЛИ ПРИНИМАТЬ ПОТОК С МИКРОФОНА
+
+ser = serial.Serial('COM0', 9600)   # НАЗНАЧАЕМ ПОРТ ВЫВОДА ИНФОРМАЦИИ НА КОНТРОЛЛЕР
+ser.open()
 
 model = Model("model-ru")           # ИНИЦИАЛИЗАЦИЯ МОДЕЛИ
 rec = KaldiRecognizer(model, 16000) # ИНИЦИАЛИЗАЦИЯ РАСПОЗНАВАНИЯ РЕЧИ
@@ -27,28 +32,32 @@ for voice in voices:
         engine.setProperty('voice', voice.id)
 
 names = ['саша','саня','сашка','сашенька','санечка','александр','железяка','консерва','бот'] #ИМЕНА НА КОТОРЫЕ РЕАГИРУЕТ АССИСТЕНТ(ДОЛЖНЫ БЫТЬ ПРОИЗНЕСЕНЫ В ЛЮБОМ МЕСТЕ В ОБРАЩЕНИИ)
-modes = ['управление','разговор']    # СПИСОК РЕЖИМОВ БОТА
+modes = ['управление','разговор']   # СПИСОК РЕЖИМОВ БОТА
 mode = modes[0]                     # ТЕКУЩИЙ РЕЖИМ РАБОТЫ
 print('ТЕКУЩИЙ РЕЖИМ РАБОТЫ:  '+ mode)
 ###########################
 
-def search_name(result,names):   #ИЩЕМ В РАСПОЗНАННОЙ ФРАЗЕ ОБРАЩЕНИЕ К АССИСТЕНТУ
+
+
+def say(text):
+    engine.say(text)
+    engine.runAndWait()              # ГОВОРИМ
+def search_name(result,names):       
     i = 0
     while i < len(result):
         if result[i] in names:
             answer(result)
             break
         else:
-            i = i + 1
+            i = i + 1    # ИЩЕМ В РАСПОЗНАННОЙ ФРАЗЕ ОБРАЩЕНИЕ К АССИСТЕНТУ
     
 
 
 def answer(result):
-   engine.say('отвечаю')
-   engine.runAndWait()
+    say('отвечаю')
 
 
-while True: #РАСПОЗНАВАНИЕ РЕЧИ И ОТПРАВКА ДАННЫХ В МЕТОД ПОИСКА СЛОВ И ОТВЕТА НА НИХ
+while True:                          #РАСПОЗНАВАНИЕ РЕЧИ И ОТПРАВКА ДАННЫХ В МЕТОД ПОИСКА ОБРАЩЕНИЯ
     data = stream.read(2000)
     if len(data) == 0:
         break
